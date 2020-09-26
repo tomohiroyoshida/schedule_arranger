d8376380832f066a99e8188f758220d377676eeb
Tomohiro Yoshida <tomohiroyoshida0511@gmail.com>
2020-09-26 20:10:17 +0900

    [WIP] デザインの追加

--- app/entry.js
+++ app/entry.js
@@ -1,5 +1,8 @@
 'use strict';
 import $ from 'jquery';
+const global = Function('return this;')();
+global.jQuery = $;
+import bootstrap from 'bootstrap'
 
 $('.availability-toggle-button').each((i, e) => {
   const button = $(e);
